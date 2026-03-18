a28bd9c0f20dbe9c86e4c35d6eeaa95e9cf4379a
Justus <jackjust@vlsi34.bu.edu>
2026-03-17 21:54:09 -0400

    make move module structural using buffers

--- sources_1/new/paramMov.v
+++ sources_1/new/paramMov.v
@@ -1,7 +1,19 @@
 module paramMov #(
-    WIDTH = 8)(
-    output wire [WIDTH-1 : 0] res,
-    input wire [WIDTH-1 :0] a
+    parameter WIDTH = 8
+)(
+    output wire [WIDTH-1:0] res,
+    input  wire [WIDTH-1:0] a
 );
-    assign res = a;
+
+    wire [WIDTH-1:0] tmp;
+
+    assign tmp = a;
+
+    genvar i;
+    generate
+        for (i = 0; i < WIDTH; i = i + 1) begin
+            buf (res[i], tmp[i]);
+        end
+    endgenerate
+
 endmodule
